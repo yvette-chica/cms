f0909cc9d72762ecc867993ae3c1c880fcefb645
yvette <yvette@its6am.com>
2020-02-24 13:11:31 +0100

    Enabled registering of a user

--- routes/home/index.js
+++ routes/home/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Post =  require('../../models/Post');
 const Category =  require('../../models/Category');
+const User =  require('../../models/User');
+const bcrypt = require('bcryptjs');
 
 router.all('/*', (req, res, next) => {
     req.app.locals.layout = 'home';
@@ -30,6 +32,51 @@ router.get('/login', (req, res) => {
 
 router.get('/register', (req, res) => {
     res.render('home/register');
+});
+
+router.post('/register', (req, res) => {
+    const errors = [];
+
+    if (!req.body.firstName) errors.push({message: 'Please add your first name'});
+    if (!req.body.lastName) errors.push({message: 'Please add your last name'});
+    if (!req.body.email) errors.push({message: 'Please add your email'});
+    if (!req.body.password) errors.push({message: 'This field cannot be blank'});
+    if (req.body.password !== req.body.passwordConfirm) errors.push({message: 'Password fields do not match'});
+
+    if (errors.length > 0) {
+        res.render('home/register', {
+            errors,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+        });
+    } else {
+        User.findOne({ email: req.body.email })
+            .then(user => {
+                if (!user) {
+                    const newUser = new User({
+                        firstName: req.body.firstName,
+                        lastName: req.body.lastName,
+                        email: req.body.email,
+                        password: req.body.password,
+                    });
+
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            newUser.password = hash;
+
+                            newUser.save().then(savedUser => {
+                                req.flash('success_message', 'You are now registered, please log in');
+                                res.redirect('/login');
+                            });
+                        });
+                    });
+                } else {
+                    req.flash('error_message', 'That email exists, please log in')
+                    res.redirect('/login');
+                }
+            });
+    }
 });
 
 router.get('/post/:id', (req, res) => {
