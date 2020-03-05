41c3a5017707ee248c004ffa57b1322e81b27148
yvette <yvette@its6am.com>
2020-03-05 16:04:02 +0100

    Add comments & categories to dashboard chart

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const faker = require('faker');
 const Post = require('../../models/Post');
+const Comment = require('../../models/Comment');
+const Category = require('../../models/Category');
 const { userAuthenticated } = require('../../helpers/authentication');
 
 router.all('/*', userAuthenticated, (req, res, next) => {
@@ -10,9 +12,17 @@ router.all('/*', userAuthenticated, (req, res, next) => {
 });
 
 router.get('/', (req, res) => {
-    Post.countDocuments().then(postCount => {
-        res.render('admin/index', { postCount });
-    });
+
+    const promises = [
+        Post.countDocuments().exec(),
+        Category.countDocuments().exec(),
+        Comment.countDocuments().exec(),
+    ];
+
+    Promise.all(promises)
+        .then(([postCount, categoryCount, commentCount]) => {
+            res.render('admin/index', { postCount, categoryCount, commentCount });
+        });
 });
 
 router.post('/generate-fake-posts', (req, res) => {
